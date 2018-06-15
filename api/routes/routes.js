b16259dbf8957542df4cb6ac568dd0ae11434531
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-06-15 09:37:36 -0700

    built out userModel, and fixed dependency issues

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,6 +3,9 @@ const { authenticate } = require('../utils/middlewares');
 const { getAllJokes, createUser, login } = require('../controllers');
 
 module.exports = server => {
+  server.get('/', function(req, res) {
+    res.send({ api: 'up and running' });
+  });
   server.get('/api/jokes', authenticate, getAllJokes);
   server.route('/api/users').post(createUser);
   server.route('/api/login').post(login);
